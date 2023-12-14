352b4cfb1ffbdbe9250ce36488ccfa96115639a9
Wama85 <wi_an9@hotmail.com>
2023-12-14 19:06:54 -0400

    Verificar que no haya corchetes

---
+++ src/corchetes.spec.js
@@ -0,0 +1,11 @@
+describe("Verificar Corchetes",()=>{
+    it("Verificar que no haya corchetes",()=>{
+        expect(verificarCorchetes('')).toEqual({})
+    })
+    
+    
+    })
+    function verificarCorchetes(cadena){   
+
+                    return {} ;
+    }
