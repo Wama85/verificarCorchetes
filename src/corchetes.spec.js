e1649e3d790593af0431dd3b0b078f3015a8bcb9
Wama85 <wi_an9@hotmail.com>
2023-12-14 19:09:43 -0400

    Verificar que hay solo un corchete y muestre incorrecto

--- src/corchetes.spec.js
+++ src/corchetes.spec.js
@@ -2,10 +2,16 @@ describe("Verificar Corchetes",()=>{
     it("Verificar que no haya corchetes",()=>{
         expect(verificarCorchetes('')).toEqual({})
     })
-    
+    it("Verificar que hay solo un corchete y muestre incorrecto",()=>{
+        expect(verificarCorchetes('[')).toEqual({'[':'Incorrecto'})
+    })
     
     })
-    function verificarCorchetes(cadena){   
-
-                    return {} ;
+    function verificarCorchetes(cadena){
+        if(cadena!==''){
+            if(cadena == '['){
+                return {'[':'Incorrecto'}
+            }   
+        }
+        return {} ;
     }
